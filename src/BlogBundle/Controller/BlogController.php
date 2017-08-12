c91de72d42a6a610238dea84d5d76a60ec739484
Dmitrij Vasiluk <vasilukwolf@gmail.com>
2017-08-12 13:43:42 +0300

    Connect first view with database

--- src/BlogBundle/Controller/BlogController.php
+++ src/BlogBundle/Controller/BlogController.php
@@ -8,7 +8,16 @@ class BlogController extends Controller
 {
     public function homepageAction()
     {
-    	return $this->render('::base.html.twig');
+        return $this->render('::base.html.twig');
+    }
+
+    public function blogViewAction($id)
+    {
+    	$em = $this->getDoctrine();
+    	$blogRepository = $em->getRepository('BlogBundle:Blog');
+    	$blog = $blogRepository->find($id);
+    	var_dump($blog);
+        return $this->render('::base.html.twig');
     }
 
 }
